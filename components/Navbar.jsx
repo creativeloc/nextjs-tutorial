edbe035572fbf4a32078e49e4fe6fa9e27319d35
creativeloc <creativeloc@yahoo.com>
2024-01-26 02:29:10 -0500

    implemented navbar component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,7 +10,7 @@ const links = [
 const Navbar = () => {
   return (
     <nav className="bg-base-300 py-4">
-      <div className="navbar px-8 max-w-6xl max-auto flex-col sm:flex-row">
+      <div className="navbar px-8 max-w-6xl mx-auto flex-col sm:flex-row">
         <Link href="/" className="btn btn-primary">
           NextJS
         </Link>
