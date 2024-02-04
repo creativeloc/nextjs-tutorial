7d084f3e498410c6e514ebcadb5eabde6b5e812f
creativeloc <creativeloc@yahoo.com>
2024-02-04 01:47:01 -0500

    implement delete action

--- components/DeleteForm.jsx
+++ components/DeleteForm.jsx
@@ -1,4 +1,13 @@
-const DeleteForm = () => {
-  return <div>DeleteForm</div>
+import { deleteTask } from "../utils/actions"
+
+const DeleteForm = ({ id }) => {
+  return (
+    <form action={deleteTask}>
+      <input type="hidden" name="id" value={id} />
+      <button className="btn btn-xs btn-error" type="submit">
+        Delete
+      </button>
+    </form>
+  )
 }
 export default DeleteForm
