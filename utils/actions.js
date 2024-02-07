47964f416554d4ec7ce9d30c35034f9f850c43f7
creativeloc <creativeloc@yahoo.com>
2024-02-07 01:32:36 -0500

    add error handling, and install zod library

--- utils/actions.js
+++ utils/actions.js
@@ -2,6 +2,7 @@
 import { revalidatePath } from "next/cache"
 import prisma from "../utils/db"
 import { redirect } from "next/navigation"
+import { z } from "zod"
 
 export const getAllTasks = async () => {
   return await prisma.task.findMany({
@@ -21,15 +22,26 @@ export const createTask = async (formData) => {
   revalidatePath("/tasks")
 }
 
-export const createTaskCustom = async (formData) => {
-  await new Promise((resolve) => setTimeout(resolve, 2000))
+export const createTaskCustom = async (prestate, formData) => {
+  // await new Promise((resolve) => setTimeout(resolve, 2000))
+
   const content = formData.get("content")
-  await prisma.task.create({
-    data: {
-      content: content
-    }
+  const Task = z.object({
+    content: z.string().min(5)
   })
-  revalidatePath("/tasks")
+  try {
+    Task.parse({ content })
+    await prisma.task.create({
+      data: {
+        content: content
+      }
+    })
+    revalidatePath("/tasks")
+    return { message: "success !!!" }
+  } catch (error) {
+    console.log(error)
+    return { message: "error..." }
+  }
 }
 
 export const deleteTask = async (formData) => {
