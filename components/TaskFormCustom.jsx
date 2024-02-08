ee868680c64b92fc563cca2cdf909d92c55acf65
creativeloc <creativeloc@yahoo.com>
2024-02-08 01:07:53 -0500

    implemented toaster notification functionality

--- components/TaskFormCustom.jsx
+++ components/TaskFormCustom.jsx
@@ -2,6 +2,8 @@
 
 import { createTaskCustom } from "../utils/actions"
 import { useFormStatus, useFormState } from "react-dom"
+import { useEffect } from "react"
+import toast from "react-hot-toast"
 
 const SubmitBtn = () => {
   const { pending } = useFormStatus()
@@ -22,10 +24,19 @@ const initialState = {
 
 const TaskForm = () => {
   const [state, formAction] = useFormState(createTaskCustom, initialState)
+  useEffect(() => {
+    if (state.message === "error") {
+      toast.error("There was an error")
+      return
+    }
+    if (state.message) {
+      toast.success("Task created successfully")
+    }
+  }, [state])
 
   return (
     <form action={formAction}>
-      {state.message ? <p className="mb-2">{state.message}</p> : null}
+      {/* {state.message ? <p className="mb-2">{state.message}</p> : null} */}
       <div className="join w-full">
         <input
           type="text"
