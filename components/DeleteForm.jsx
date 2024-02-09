dda2d6b7caee9399b95000ceca8d6eb22cb8e813
creativeloc <creativeloc@yahoo.com>
2024-02-09 01:00:41 -0500

    add pending functionality to delete button

--- components/DeleteForm.jsx
+++ components/DeleteForm.jsx
@@ -1,12 +1,22 @@
+"use client"
+
 import { deleteTask } from "../utils/actions"
+import { useFormStatus } from "react-dom"
+
+const SubmitButton = () => {
+  const { pending } = useFormStatus()
+  return (
+    <button className="btn btn-xs btn-error" disabled={pending} type="submit">
+      {pending ? "pending..." : "Delete"}
+    </button>
+  )
+}
 
 const DeleteForm = ({ id }) => {
   return (
     <form action={deleteTask}>
       <input type="hidden" name="id" value={id} />
-      <button className="btn btn-xs btn-error" type="submit">
-        Delete
-      </button>
+      <SubmitButton />
     </form>
   )
 }
