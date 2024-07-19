2a2b8f2fcdda1514adf124acb85082cbf6639e86
creativeloc <creativeloc@yahoo.com>
2024-07-19 06:04:14 -0400

    display list and individual drinks

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,5 +1,26 @@
-const SingleDrinkPage = ({ params }) => {
-  console.log(params.id)
-  return <div>SomeDrinkPage</div>
+import Link from "next/link"
+const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i="
+
+const getSingleDrink = async (id) => {
+  const res = await fetch(`${url}${id}`)
+  if (!res.ok) {
+    throw new Error("Failed to fetch a drink...")
+  }
+  return res.json()
+}
+
+const SingleDrinkPage = async ({ params }) => {
+  const data = await getSingleDrink(params.id)
+  const title = data?.drinks[0]?.strDrink
+  const imgSrc = data?.drinks[0]?.strDrinkThumb
+  console.log(title, imgSrc)
+  return (
+    <div>
+      <Link href="/drinks" className="btn btn-primary mt-8 mb-12">
+        Back to Drinks
+      </Link>
+      <h2 className="text-4xl mb-8">{title}</h2>
+    </div>
+  )
 }
 export default SingleDrinkPage
