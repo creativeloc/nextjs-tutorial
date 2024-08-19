6407f2dd9de4ea4534bf8bd1fee34bcb5a2bbbef
creativeloc <creativeloc@yahoo.com>
2024-08-19 06:22:51 -0400

    resume L38 @4:24

--- app/tasks/[id]/page.js
+++ app/tasks/[id]/page.js
@@ -10,8 +10,8 @@ const SingleTaskPage = async ({ params }) => {
         <Link href="/tasks" className="btn btn-accent">
           Back to tasks
         </Link>
-        <EditForm task={task} />
       </div>
+      <EditForm task={task} />
     </>
   )
 }
--- components/EditForm.jsx
+++ components/EditForm.jsx
@@ -1,5 +1,36 @@
+import { editTask } from "@/utils/actions"
+
 const EditForm = ({ task }) => {
-  console.log(task)
-  return <div>EditForm</div>
+  const { id, completed, content } = task
+
+  return (
+    <form
+      action={editTask}
+      className="max-w-sm p-12 border border-base-300 rounded-lg"
+    >
+      <input type="hidden" name="id" value={id} />
+      {/* content */}
+      <input
+        type="text"
+        required
+        defaultValue={content}
+        name="content"
+        className="input input-bordered w-full"
+      />
+      {/* completed */}
+      <div className="form-control">
+        <label htmlFor="completed" className="label cursor-pointer">
+          <span className="label-text">Completed</span>
+          <input
+            type="checkbox"
+            defaultChecked={completed}
+            name="completed"
+            id="completed"
+            className="checkbox checkbox-primary checkbox-sm"
+          />
+        </label>
+      </div>
+    </form>
+  )
 }
 export default EditForm
